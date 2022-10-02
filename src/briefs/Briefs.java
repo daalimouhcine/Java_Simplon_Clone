9eccc6012dc0d94df685d80f0e8c7c68173327dd
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-10-02 15:31:38 +0100

    modify briefs class

--- src/briefs/Briefs.java
+++ src/briefs/Briefs.java
@@ -6,10 +6,27 @@ import java.util.ArrayList;
 
 public class Briefs {
     public String id;
-    public int promoId;
+    public String promoId;
     public String title;
     public String description;
+    public ArrayList<String> technologies;
     public boolean launch;
     public ArrayList<ValidationMessage> valide;
+
+    Briefs(String id, String promoId, String title, String description, ArrayList<String> technologies) {
+        this.id = id;
+        this.promoId = promoId;
+        this.title = title;
+        this.description = description;
+        this.technologies = technologies;
+        this.launch = false;
+    }
+
+    public void launchTheBrief() {
+        this.launch = true;
+    }
+
+
+
 
 }
